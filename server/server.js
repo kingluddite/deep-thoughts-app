36b8f7c3cc9535d6b79164364c016ac2fd0816dc
philip howley <howley.phil@gmail.com>
2021-01-01 16:08:05 -0500

    Add mutations

--- server/server.js
+++ server/server.js
@@ -4,6 +4,8 @@ const { ApolloServer } = require("apollo-server-express");
 
 // import our typeDefs and resolvers
 const { typeDefs, resolvers } = require("./schemas");
+// import middleware
+const { authMiddleware } = require("./utils/auth");
 const db = require("./config/connection");
 
 const PORT = process.env.PORT || 3001;
@@ -12,8 +14,8 @@ const app = express();
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  context: authMiddleware,
 });
-
 // integrate our Apollo server with the Express application as middleware
 server.applyMiddleware({ app });
 
